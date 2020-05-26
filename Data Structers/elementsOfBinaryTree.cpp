3ec0d404f11a6c15ea4b81f45bb842a876bde8ca
Sezer <30723634+sezermehmed@users.noreply.github.com>
2020-05-26 13:38:36 +0300

    Create elementsOfBinaryTree.cpp

---
+++ Data Structers/elementsOfBinaryTree.cpp
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<stdio.h>
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int countNodes(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    else {
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+}
+
+int  main() {
+    countNodes;
+    return 0;
+}
